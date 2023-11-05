dc8d48a5e82ccebdcc4f95c4ced517db152574e4
Nathaly Villamor <nath@Nathalys-MBP.lan>
2023-11-05 18:08:48 -0300

    adding point 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :user_quota
 
   def user_quota
-    response = CountHits.call(user: user)
+    response = CountHits.call(user:)
 
-    if not response.success?
-      render json: { error: response.message }
-    end
+    return if response.success?
+
+    render json: { error: response.message }
   end
 
   private
 
   def user
-    id = params.require("user_id")
+    id = params.require('user_id')
     User.find(id)
   end
 end
--- app/interactors/count_hits.rb
+++ app/interactors/count_hits.rb
@@ -1,18 +1,23 @@
 class CountHits
   include Interactor
 
+  MAX_HITS = 10
+
   def call
     count = current_count
-    if count < 10
+    if count < MAX_HITS
       context.count = count
     else
-      context.fail!(message: "over quota")
+      context.fail!(message: 'over quota')
     end
   end
+
+  private
 
   def current_count
     current_month = Time.zone.now.month
     current_year = Time.zone.now.year
+
     hit = context.user.hits.where(month: current_month, year: current_year).last
     hit.current_count
   end
